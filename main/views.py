f865b1e22480359fb8e6ee220ab6dbc0cbdab231
nelitovec <154272838+msfelzhel@users.noreply.github.com>
2024-12-09 00:51:27 +0300

    0.4

--- main/views.py
+++ main/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Главная',
+        'title': 'Burnycom - Главная',
         'content': 'Главная страница'
     }
     return render(request, 'main/index.html',context)
+
+def matem(request):
+    context = {
+        'title': 'Burnycom - Математика',
+        'content': 'Математика'
+    }
+    return render(request, 'main/matem.html' ,context)
