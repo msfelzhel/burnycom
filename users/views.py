3309de651d2cca0732f8f58a398fd71c08db9ef1
nelitovec <154272838+msfelzhel@users.noreply.github.com>
2024-12-11 01:47:52 +0300

    7.1

--- users/views.py
+++ users/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 def login(request):
+    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+    if form.is_valid():
+        username = request.POST['username']   
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        if user:
+            auth.login(request,user)
+            return HttpResponseRedirect(reverse('main:index'))
+        else:
+            form = UserLoginForm()  
+    
     context = {
         'title': 'Burnycom - Вход',
-        'content': 'Вход'
+        'content': 'Вход',
+        'form': form
     }
     return render(request, 'users/login.html',context)
 
