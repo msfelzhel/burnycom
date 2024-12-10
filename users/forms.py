34d658aa08536852737216be95c4eb01d6d2f711
nelitovec <154272838+msfelzhel@users.noreply.github.com>
2024-12-11 02:25:19 +0300

    0.72

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,19 @@
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget= forms.TextInput(attrs= {"autofocus":True,
+                                'class' : 'form-control border-0',
+                                }))
+    password = forms.CharField(
+        widget= forms.PasswordInput(attrs= {"autocomplete":"current-password",
+                                'class' : 'form-control pr-11 border-0',
+                                }))
+    
+    
     class Meta:
         model = User()
+        fields = ['username','password']
