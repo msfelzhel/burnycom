b1d760661dc340443d659e409bc71c77d60c16e1
nelitovec <154272838+msfelzhel@users.noreply.github.com>
2024-12-17 02:54:06 +0300

    0.97

--- lk/urls.py
+++ lk/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from lk import views
+from django.contrib.auth.views import LogoutView
 
 app_name = 'lk'
 
 urlpatterns = [
-    path('',views.lk,name='lk')  
+    path('',views.lk,name='lk'),
+    path('logout/',LogoutView.as_view(next_page='/'),name='logout')  
 ]
